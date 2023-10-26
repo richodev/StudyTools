60333774a120351381263907c96b77c60c5fee90
richodev <oliver.richter@student.uni-tuebingen.de>
2023-10-26 08:15:24 +0200

    Added notion database query for retrieving page id and HttpClient for handling HTTP requests and responses.

---
+++ ScriptToNotionExtractor/Notion/HttpClient.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+import requests
+import json
+
+class HttpClient:
+    def __init__(self, authToken: str, defaultHeaders: dict = {}):
+        authHeader = { "Authorization": f"Bearer {authToken}" }
+        defaultHeaders.update(authHeader)
+        self.__defaultHeaders = defaultHeaders.copy()
+
+    def RunGet(self, url: str, data: dict = None, params: dict = None, headers: dict = None) -> Any:
+        response = self.__ExecuteRequest("GET", url, json=data, params=params, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"[HttpClient][ERROR] GET request to url {url} returned with status code {response.status_code}.")
+        if response.headers.get("content-type", None) == None:
+            return response.content
+        if "application/json" in response.headers["content-type"]:
+            return json.loads(response.content)
+        return response
+    
+    def RunPost(self, url: str, data: dict = None, params: dict = None, headers: dict = None) -> Any:
+        response = self.__ExecuteRequest("POST", url, json=data, params=params, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"[HttpClient][ERROR] POST request to url {url} returned with status code {response.status_code}.")
+        if response.headers.get("content-type", None) == None:
+            return response.content
+        if "application/json" in response.headers["content-type"]:
+            return json.loads(response.content)
+        return response
+
+    def __ExecuteRequest(self, requestType: str, url: str, **kwargs) -> requests.Response:
+        self.__defaultHeaders.update(kwargs.get("headers", {}))
+        kwargs["headers"] = self.__defaultHeaders
+        return requests.request(requestType, url, **kwargs)
--- ScriptToNotionExtractor/Notion/NotionConnector.py
+++ ScriptToNotionExtractor/Notion/NotionConnector.py
@@ -1,6 +1,7 @@
-from typing import List
+from HttpClient import HttpClient
 
-import requests
+from typing import List, Dict
+
 import json
 import os
 
@@ -10,21 +11,51 @@ class NotionConnector(object):
             secretToken = os.environ.get("NOTION_TOKEN")
         if secretToken is None:
             raise PermissionError("Notion secret token is not defined.")
-        self.__headers = {
-            "Authorization": f"Bearer {secretToken}",
-            "Notion-Version": "2022-06-28"
-        }
+        self.__httpClient = HttpClient(secretToken, defaultHeaders={"Notion-Version": "2022-06-28" })
 
     def SearchPageId(self, pageName: str) -> List[str]:
-        self.__headers["Content-Type"] = "application/json"
-        response = requests.post("https://api.notion.com/v1/search", headers=self.__headers, json={
+        headers = {"Content-Type": "application/json"}
+        content = self.__httpClient.RunPost("https://api.notion.com/v1/search", headers=headers, data={
             "query": pageName,
             "sort": {"direction": "descending", 
                      "timestamp": "last_edited_time"}
         })
-        response.raise_for_status()
-        return json.loads(response.content)["results"]
+        return content["results"]
+    
+    def CreatePage(self, parentId: str, pageTitle: str) -> str:
+        headers = {"Content-Type": "application/json"}
+        content = self.__httpClient.RunPost("https://api.notion.com/v1/***", headers=headers, data={
+            "parent": {"page_id": parentId},
+            "pageTitle": pageTitle
+        })
+        return content["result"]
+    
+    def GetPageIdByPropertyValue(self, databaseId: str, propertyName: str, propertyValue: str) -> str:
+        url = f"https://api.notion.com/v1/databases/{databaseId}/query"
+        headers = {"Content-Type": "application/json"}
+        content = self.__httpClient.RunPost(url, headers=headers, data={
+            "filter": {
+                "property": propertyName,
+                "rich_text": {
+                    "equals": propertyValue
+                }
+            }
+        })
+        results = content["results"]
+        resultType = content["object"]
+        if resultType == "list" and len(results) > 1:
+            raise Exception(f"Found more than one page with the property value {propertyValue}.")
+        return results[0]["id"]
+    
+    def GetPage(self, pageId: str) -> Dict:
+        url = f"https://api.notion.com/v1/pages/{pageId}"
+        content = self.__httpClient.RunGet(url, headers=self.__headers)
+        return content
+        
 
 notionConnector = NotionConnector()
-siteId = notionConnector.SearchPageId("Modellierung und Simulation 1")
+siteId = notionConnector.SearchPageId("Theoretische Informatik: Algorithmen Datenstrukturen")
+print(f"Found Sites: {len(siteId)}. Showing Last Two Edited Sites:")
+print(json.dumps(siteId[:2], indent=4))
+siteId = notionConnector.GetPageIdByPropertyValue("047a0500-c37f-4d15-bf29-c19e1a11538e", "Kursnummer", "INFO4345")
 print(json.dumps(siteId, indent=4))
